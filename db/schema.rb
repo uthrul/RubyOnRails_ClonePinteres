581e55feeb2f4505ffe099cb5cbbe3e1f665b5af
saud maruli <uthrul@gmail.com>
2018-09-07 21:58:32 +0700

    create pins scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180907050024) do
+ActiveRecord::Schema.define(version: 20180907123644) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ test/system/pins_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class PinsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit pins_url
+  #
+  #   assert_selector "h1", text: "Pin"
+  # end
+end
