d8a2426faab53f8d3bb6c4feb5730cac3caaddc3
saud maruli <uthrul@gmail.com>
2018-09-18 15:15:55 +0700

    update user name profile user

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -47,7 +47,7 @@ class PinsController < ApplicationController
     end
 
     def pin_params
-      params.require(:pin).permit(:description, :image)
+      params.require(:pin).permit(:description, :image, :name)
     end
 
 
